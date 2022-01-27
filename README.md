bd67e7dd46924045787d9b59f376786078f957f7
TimothyElems <85181892+TimothyElems@users.noreply.github.com>
2022-01-27 05:25:54 +0100

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Alphabin
+
+On my birthday, my sister sent me a message and said, 'Timmy, decode this.' I flatly replied, 'nah.'
+It was in binary and I don't do binary. A few weeks later I'm going through the Python documentation for a GUI-based project, and I saw something about binary. 
+I decided to embark on this project.
+
+## What Is It
+Alphabin is a program that converts alpha-numeric strings into binary strings, and reverse. 
+
+## What I Learned Through This Project
+- for Loops. Prior to this project, I had not done any that utilized loops. The for loop is essential to the main method of this program.
+- 
+
+## Resources And Sources
+
+***  chr(int('01001000', 2))
+'H'
---
+++ inglish_to_bin.py
@@ -0,0 +1,56 @@
+'''
+*all good programmers start with documentation
+
+I want to convert Inglish to Binary
+
+Convert a binary number, 0b01001000 to an alphabet or string, through chr()
+Pseudocode:\
+    - Start with a variable that is a string (later, make it an input)
+    - Convert the string to an integer
+    - Convert the integer to binary
+    - Loop the operation
+
+
+I need to loop something. A loop is a condition + clause(s).
+What I need is for the program to continually take input and turn that into binary code. The program will also need a way to stop this process.\
+    Condition: As long as the input isn't 'quit' or 'Quit', run the loop; or else, end the loop.
+    Clause: Turn the alphanumeric characters into binary chars.
+'''
+
+def ing_2_bin():
+    print('Hello!\nType a message and you\'ll see it in binary.\nType "quit" to end the program.')
+    dope = ()
+    # if dope != 'quit' or 'Quit':
+    for i in dope:
+        dope2num = str(i)
+        num2bin = ord(dope2num)
+        binned = bin(num2bin)
+        # print(binned)
+           
+            # IT WORKS!!!!!!!!!!!!!!!!!!!!!!!! BOOOWOOOOM
+   
+        
+
+
+
+# ing_2_bin() 
+
+
+def run_up():
+    apple = input()
+    if apple == 'yes':
+        print('Thanks')        
+    else:    
+        for i in range(0, 60):
+            print(i)
+        # dapple = input()
+        apple = input()
+        print('Do you want to try again? Yes or no:', apple)
+        
+
+run_up()
+
+# *** Ta-Da List ***
+# What is the iterative block?
+# What initiates the loop?
+# What ends the loop?
